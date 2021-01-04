d37422bdcee0ad3e265a3e9460209dcafd63f2ce
nadiakostiv <nadiakostiv98@gmail.com>
2021-01-03 23:40:33 -0500

    Update image recognition for red object

--- openMV/image_recognition.py
+++ openMV/image_recognition.py
@@ -6,11 +6,40 @@
 # only track colored objects which have both the colors below in them.
 
 import sensor, image, time, math
+# from pyb import UART
+from pyb import LED
 
+# Distance Constants
+lens_mm = 2.8 # standard lens
+height_obj_mm = 30.0
+image_height_pixels = 240
+image_width_pixels = 340
+sensor_w_mm = 3.984
+sensor_h_mm = 2.952
+offset_mm = 100.0 # offset fix
+
+h_fov = 2 * math.atan((sensor_w_mm/2) / lens_mm)
+v_fov = 2 * math.atan((sensor_h_mm/2) / lens_mm)
+print("h_fov: ", h_fov, ", v_fov: ", v_fov)
+
+#Distance func
+def distance_to_obj(h): # h = blob.h()
+    return ((lens_mm * height_obj_mm * image_height_pixels) / (h * sensor_h_mm))
+
+blue_led  = LED(3)
+green_led  = LED(2)
+red_led  = LED(1)
+
+# uart = UART(3, 115200, timeout_char = 1000)
+blue_led.on()
 # Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
 # The below thresholds track in general red/green things. You may wish to tune them...
-thresholds = [(30, 100, 15, 127, 15, 127), # generic_red_thresholds -> index is 0 so code == (1 << 0)
-              (30, 100, -64, -8, -32, 32)] # generic_green_thresholds -> index is 1 so code == (1 << 1)
+#thresholds = [(30, 100, 15, 127, 15, 127), # generic_red_thresholds -> index is 0 so code == (1 << 0)
+#              (30, 100, -64, -8, -32, 32)] # generic_green_thresholds -> index is 1 so code == (1 << 1)
+
+thresholds = [(55, 100,-24, 11, 32, 86),     #1#yellow
+              (0, 39, -82, 127, 14, 127),     #2#red
+              (39, 100,-51,-12, 10, 57)]      #4#green
 # Codes are or'ed together when "merge=True" for "find_blobs".
 
 sensor.reset()
@@ -21,35 +50,63 @@ sensor.set_auto_gain(False) # must be turned off for color tracking
 sensor.set_auto_whitebal(False) # must be turned off for color tracking
 clock = time.clock()
 
+blue_led.off()
+green_led.off()
+red_led.off()
+
+object_x_old = 0
+object_y_old = 0
+
+code = 2 ## 1:yellow   2:red    4:green
+buf = "00"
 # Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
 # returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
 # camera resolution. "merge=True" must be set to merge overlapping color blobs for color codes.
-lens_mm = 2.8 # Standard Lens.
-target_object_height_mm = 100 # NEED TO MODIFY PER OBJECT
-image_height_pixels = 240.0 # QVGA
-sensor_h_mm = 2.952 # For OV7725 sensor - see datasheet.
-offest_mm = 100.0 # Offset fix...
-def blob_to_distance(blob): # r == (x, y, w, h) -> r[3] == h
-    return ((lens_mm * average_head_height_mm * image_height_pixels) / (blob.h * sensor_h_mm)) - offest_mm
 
 while(True):
     clock.tick()
+
+    blue_led.off()
+    green_led.off()
+    red_led.off()
+
     img = sensor.snapshot()
     for blob in img.find_blobs(thresholds, pixels_threshold=100, area_threshold=100, merge=True):
-        if blob.code() == 3: # r/g code == (1 << 1) | (1 << 0)
-            # These values depend on the blob not being circular - otherwise they will be shaky.
-            if blob.elongation() > 0.5:
-                img.draw_edges(blob.min_corners(), color=(255,0,0))
-                img.draw_line(blob.major_axis_line(), color=(0,255,0))
-                img.draw_line(blob.minor_axis_line(), color=(0,0,255))
-            # These values are stable all the time.
+#check which color should be detected
+        if uart.any()>0 :
+            buf=uart.read()
+            print (buf[0])
+            if buf[0]==ord('y') :
+                code = 1
+            if buf[0]==ord('r') :
+                code = 2
+            if buf[0]==ord('g') :
+                code = 4
+
+#check if there is object with right color
+        if blob.code() == code:
+
             img.draw_rectangle(blob.rect())
             img.draw_cross(blob.cx(), blob.cy())
-            # Note - the blob rotation is unique to 0-180 only.
-            img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)			
-            # get distance
-            obj_distance = blob_to_distance(blob)
-            break # only get one blob?
 
-    print(clock.fps())
+#make sure the detected object is stable and print the coordinates
+#first it detect if the coordinates of blob is available
+#second compared with the last position to make sure if the object is not moving
+#third reduce the affect of anbience
+            if blob.cx()!=None and (
+                abs(object_x_old - int(blob.cx())) < 8 and
+                abs(object_y_old - int(blob.cy())) < 8) and (
+                blob.w()>35 and
+                blob.h()>35):
+               #just detect the objects. turn on the blue only
+               blue_led.on()
+               red_led.off()
+               green_led.off()
+               print("stable!")
+               print("blob.cx, blob.cy, blob.w, blob.h are: ")
+               print(blob.cx(), blob.cy(),blob.w(), blob.h())
+               print("distance: ", distance_to_obj(blob.h()))
+               #print (buf)
 
+            object_x_old = int(blob.cx())
+            object_y_old = int(blob.cy())
