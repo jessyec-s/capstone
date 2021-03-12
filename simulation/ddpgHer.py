bfb3559dc938bb0618d84ae8598f21d816cfef44
Jessye Coleman-Shapiro <cjessye@gmail.com>
2021-03-12 11:58:19 -0500

    Fix simulation run loop

--- simulation/ddpgHer.py
+++ simulation/ddpgHer.py
@@ -18,35 +18,32 @@ class DDPG_HER:
                          goal_selection_strategy=self.goal_selection_strategy,buffer_size=1000000,
                          batch_size=256, gamma=.95, learning_rate=1e-3, verbose=1, max_episode_length=50)
 
-    def run(self,epochs=500,load_checkpoints=False):
-        #obs = self.env.get_observation()
-        #print("OBS: ", obs)
-        # print("np.array(obs).shape: ", obs.shape)
-        print("observation_space: ", self.env.observation_space)
+    def run(self, epochs=5000, train=True):
         # Train the model
-        self.model.learn(total_timesteps=250000)
-        self.model.save("./her_bit_env")
+        if train:
+            # 1000 epochs is approximately 50,000 time steps
+            self.model.learn(total_timesteps=(50000 * epochs))
+            self.model.save("./her_bit_env")
 
         # WARNING: you must pass an env
         # or wrap your environment with HERGoalEnvWrapper to use the predict method
         self.model = HER.load('./her_bit_env', env=self.env)
 
-        obs = self.env.reset()
-        
-        print("OBS: ", obs)
-        score=0
         success_rate = []
-        for i in range(1):
-            for _ in range(1000):
+        for i in range(10):
+            obs = self.env.reset()
+            score = 0
+            for j in range(1000):
                 action, _ = self.model.predict(obs)
 
                 obs, reward, done, info = self.env.step(action)
-                score+=reward
+                score += reward
                 success_rate.append(info["is_success"])
                 self.env.render()
                 if done:
-                    return
-            print("epoch: ",i) 
-            print("score:", score, "average score:", score / i)
+                    break
+                print("epoch: ", j)
+                print("score:", score, "average score:", score / j)
+            print("success rate: ", success_rate.count(True) / len(success_rate))
         
 
