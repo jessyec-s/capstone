e2cedc6d891eb9e74803c29a49b2498b2fe88612
Jessye Coleman-Shapiro <cjessye@gmail.com>
2021-03-26 18:18:37 -0400

    add test main func call

--- openMV/uarmTests.py
+++ openMV/uarmTests.py
@@ -6,6 +6,10 @@ ANGLE_LIMIT = (0., 180.)
 HEIGHT_LIMIT = (0., 150.)
 
 
+def run_tests():
+    determine_success_rate()
+
+
 def determine_success_rate(num_iters=50):
     locations = []
     for i in range(num_iters):
@@ -13,3 +17,6 @@ def determine_success_rate(num_iters=50):
                          random.randrange(ANGLE_LIMIT[0], ANGLE_LIMIT[1]),
                          random.randrange(HEIGHT_LIMIT[0], HEIGHT_LIMIT[1]))
     ddpg_loop_no_camera(locations)
+
+
+run_tests()
